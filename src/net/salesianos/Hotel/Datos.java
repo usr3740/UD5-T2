0bfd1541b4a2ddfa86a1f89c4de5a36077bc8b9f
USR3740Gabriel <usr3740@salesianos-lacuesta.net>
2026-04-27 09:39:44 +0100

    Creamos las clases para recojer los datos de los hoteles

---
+++ src/net/salesianos/Hotel/Datos.java
@@ -0,0 +1,20 @@
+package net.salesianos.Hotel;
+
+public class Datos {
+
+    private String Nombre;
+    private int Etrellas;
+    private int Puntuacion;
+    private int Precio;
+    private String Ubicacion;
+
+    public Datos(String Nombre, int Estrellas, int Puntuacion, int Precio, String Ubicacion) {
+        this.Nombre = Nombre;
+        this.Etrellas = Estrellas;
+        this.Puntuacion = Puntuacion;
+        this.Precio = Precio;
+        this.Ubicacion = Ubicacion;
+
+    }
+
+}
