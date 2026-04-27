9c405e8bff7e9af46c2cf1105948b8b0b2eeae43
USR3740Gabriel <usr3740@salesianos-lacuesta.net>
2026-04-27 12:00:40 +0100

    Creamos una clase para validar los diferentes datos que les pedimos a los hoteles

--- src/net/salesianos/utilidades/Utilidades.java
+++ src/net/salesianos/utilidades/Utilidades.java
@@ -14,4 +14,33 @@ public class Utilidades {
         return txt;
     }
 
+    public static int pedirEntero(String mensaje) {
+        while (true) {
+            try {
+                String input = JOptionPane.showInputDialog(mensaje);
+                if (input == null)
+                    return -1;
+                return Integer.parseInt(input);
+            } catch (Exception e) {
+                JOptionPane.showConfirmDialog(null, "Introduce un numero valido");
+            }
+        }
+    }
+
+    public static int PedirPuntuacion() {
+        while (true) {
+            try {
+                String input = JOptionPane.showInputDialog("Puntuación del 1 al 5: ");
+                if (input == null)
+                    return -1;
+                int p = Integer.parseInt(input);
+                if (p >= 1 && p <= 5)
+                    return p;
+            } catch (Exception ignored) {
+            }
+            JOptionPane.showConfirmDialog(null, "Introduce un numero del 1 al 5");
+
+        }
+    }
+
 }
